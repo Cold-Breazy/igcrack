64a0712584f2416acbdf19395ceaa0bf4dd9b4b6
Breazy <101399227+Cold-Breazy@users.noreply.github.com>
2022-03-15 15:00:50 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -15,12 +15,35 @@ pip3 install requests --upgrade
 pip3 install requests[socks]
 pip3 install stem
 pip3 install instagram-py
+cd $HOME/igcrack
+echo "Now Extracting Files...!!"
+echo
+tar -xvf core.tar.gz
+tar -xvf main.tar.gz
+tar -xvf pass.tar.gz
+tar -xvf stylesheets.tar.gz
+clear
+echo "now removing unnecessary Files .....!"
+echo
+rm -rf core.tar.gz
+rm -rf main.tar.gz
+rm -rf pass.tar.gz
+rm -rf stylesheets.tar.gz
+clear
+echo "installing config.json....!"
+echo
 cd $HOME
 wget -O ~/instapy-config.json "https://git.io/v5DGy"
-cd $HOME/ighack
+cd $HOME/igcrack
 cd /data/data/com.termux/files/usr/etc/tor
 rm torrc
 cd $HOME/igcrack
 mv torrc /data/data/com.termux/files/usr/etc/tor
 cd $HOME
 tor
+clear
+echo "installation complete👌 You can Now Run The Tool"
+echo
+sleep 4
+clear
+exit
